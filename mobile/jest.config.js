fa9e88ece72d67261c409ca6b37e184a6557b056
AMVamsi <amvamsi1308@gmail.com>
2026-05-17 00:28:18 +0200

    test(#59)[log:manual]: add Jest tests for bbox scaling and diary store

---
+++ mobile/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'jest-expo',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts', '!src/**/index.ts'],
+  coverageThreshold: {
+    global: { lines: 20 },
+  },
+};
